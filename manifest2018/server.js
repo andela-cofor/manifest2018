d13d72a2a6cfa65681016252352a7ca623cddbea
Ofor Chinedu <oforchinedu@Ofors-MacBook-Pro.local>
2017-12-05 17:53:13 +0100

    finished

--- manifest2018/server.js
+++ manifest2018/server.js
@@ -6,4 +6,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/*', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
 
-app.listen(8080, () => console.info('started on 8080'));
+app.listen(process.env.PORT || 8080, () => console.info('started on 8080'));
